77564d21a740b93d1ac8080b172f7e0734d5005d
afahernandes <afahernandes@gmail.com>
2021-08-20 19:44:53 +0700

    2. Register

--- src/component/navbars/Header.js
+++ src/component/navbars/Header.js
@@ -3,13 +3,18 @@ import { Navbar, Container, Nav, Button, NavLink } from "react-bootstrap";
 import Icon from "../../assets/brandlogo.svg";
 import LoginModal from "../modals/LoginModal";
 import { useState } from "react";
+import RegisterModal from "../modals/RegisterModal";
 
 
 function Header() {
   const [show, setShow] = useState(false);
+  const [showRegis, setShowRegis] = useState(false);
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const handleShowRegis = () => setShowRegis(true);
+  const handleCloseRegis = () => setShowRegis(false);
 
   return (
     <div>
@@ -25,12 +30,13 @@ function Header() {
             <Nav className="me-auto"></Nav>
             <Nav>
               <Button className="button2" onClick={handleShow}>Login</Button>
-              <Button className="button1">Register</Button>
+              <Button className="button1" onClick={handleShowRegis} >Register</Button>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <LoginModal show={show} handleClose={handleClose}/>
+      <RegisterModal show={showRegis} handleClose={handleCloseRegis}/>
     </div>
   );
 }

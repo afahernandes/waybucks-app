0000ca9619f14e28a2dc968d31823030889cd4d1
afahernandes <afahernandes@gmail.com>
2021-08-20 22:20:32 +0700

    3. Landing Page

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Banner from "../assets/banner.svg";
+import CardList from "../component/CardList";
+import data from "../data/dataproduk";
+function Home() {
+  return (
+    <div>
+      <Container>
+        
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <img src={Banner} alt="v" style={{ width: "100%" }} />
+            <h3 className="header3">Let’s Order</h3>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container style={{marginBottom:'20px'}}>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <CardList data={data} />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
